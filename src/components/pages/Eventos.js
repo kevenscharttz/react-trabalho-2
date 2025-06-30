1d505f251355120cecfc04f691583200ffb1dac9
Hiago-Cardoso-Felisberto <hiagocf2003@gmail.com>
2025-06-29 22:29:13 -0300

    Correções ev.

--- src/components/pages/Eventos.js
+++ src/components/pages/Eventos.js
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
-function Eventos(){
-    return <h1>Eventos</h1>
-=======
-import styles from './Eventos.module.css'
+// import styles from './Eventos.module.css'
 import EventosForm from '../project/EventosForm'
 
 function Eventos(){
     return (
         <>
-            <EventosForm btnText="Criar Projeto"/>
+            <EventosForm btnText="Criar Projeto" />
         </>
 
     )
->>>>>>> 88d6cba812f564e467d0296b66521f1328a51727
 
 }
-
-
-<<<<<<< HEAD
 export default Eventos
-=======
-export default Usuarios
->>>>>>> 88d6cba812f564e467d0296b66521f1328a51727
